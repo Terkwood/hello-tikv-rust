1daabd8c41fa9ca1d37a4539b0d9844d4e19341c
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-11 06:37:35 -0500

    write something

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use tikv_client::{Config, RawClient, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let config = Config::default();
+    let client = RawClient::new_with_config(vec!["127.0.0.1:2379"], config).await?;
+    client.put("key".to_owned(), "value".to_owned()).await?;
+    let value = client.get("key".to_owned()).await.unwrap().unwrap();
+    println!("Hello, TiKV! {}", String::from_utf8(value).unwrap());
+    Ok(())
 }
